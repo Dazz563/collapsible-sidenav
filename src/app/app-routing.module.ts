926a25dd2d4da9b469ecd6a72dffeebf5aeda92c
Darren <58746026+Dazz563@users.noreply.github.com>
2023-11-24 14:26:16 +0200

    initial template complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './sidenav-menu/dashboard/dashboard.component';
+import {ProductsComponent} from './sidenav-menu/products/products.component';
+import {CategoriesComponent} from './sidenav-menu/categories/categories.component';
+import {OrdersComponent} from './sidenav-menu/orders/orders.component';
+import {SettingsComponent} from './sidenav-menu/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: '/dashboard',
+		pathMatch: 'full',
+	},
+	{
+		path: 'dashboard',
+		component: DashboardComponent,
+	},
+	{
+		path: 'products',
+		component: ProductsComponent,
+	},
+	{
+		path: 'categories',
+		component: CategoriesComponent,
+	},
+	{
+		path: 'orders',
+		component: OrdersComponent,
+	},
+	{
+		path: 'settings',
+		component: SettingsComponent,
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SidenavMenuComponent} from './sidenav-menu/sidenav-menu.component';
+import {DashboardComponent} from './sidenav-menu/dashboard/dashboard.component';
+import { ProductsComponent } from './sidenav-menu/products/products.component';
+import { CategoriesComponent } from './sidenav-menu/categories/categories.component';
+import { OrdersComponent } from './sidenav-menu/orders/orders.component';
+import { SettingsComponent } from './sidenav-menu/settings/settings.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent, //
+		SidenavMenuComponent,
+		DashboardComponent,
+  ProductsComponent,
+  CategoriesComponent,
+  OrdersComponent,
+  SettingsComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
