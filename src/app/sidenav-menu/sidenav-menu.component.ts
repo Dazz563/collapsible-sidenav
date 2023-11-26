066a3b401371db0f1327e9bc6faf65eca11584f4
Darren <58746026+Dazz563@users.noreply.github.com>
2023-11-26 11:09:56 +0200

    update

--- src/app/sidenav-menu/sidenav-menu.component.ts
+++ src/app/sidenav-menu/sidenav-menu.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {DarkModeService} from '../services/dark-mode.service';
-import {Subscription} from 'rxjs';
 
 @Component({
 	selector: 'app-sidenav-menu',
